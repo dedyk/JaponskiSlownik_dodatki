3dc64083d384d0ad79cea2f42a4367f497e1a8e7
dedyk <dedykjp@gmail.com>
2015-12-18 22:25:16 +0100

    tanos

---
+++ tanos/source/update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+wget http://www.tanos.co.uk/jlpt/jlpt5/vocab/VocabList.N5.doc
+wget http://www.tanos.co.uk/jlpt/jlpt4/vocab/VocabList.N4.doc
+wget http://www.tanos.co.uk/jlpt/jlpt3/vocab/VocabList.N3.doc
+wget http://www.tanos.co.uk/jlpt/jlpt2/vocab/VocabList.N2.doc
+wget http://www.tanos.co.uk/jlpt/jlpt1/vocab/VocabList.N1.doc
+
+wget http://www.tanos.co.uk/jlpt/jlpt5/kanji/KanjiList.N5.doc
+wget http://www.tanos.co.uk/jlpt/jlpt4/kanji/KanjiList.N4.doc
+wget http://www.tanos.co.uk/jlpt/jlpt3/kanji/KanjiList.N3.doc
+wget http://www.tanos.co.uk/jlpt/jlpt2/kanji/KanjiList.N2.doc
+wget http://www.tanos.co.uk/jlpt/jlpt1/kanji/KanjiList.N1.doc
