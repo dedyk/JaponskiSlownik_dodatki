a6679db769eb8c0b0904407e9b0ba0892f9546a2
Fryderyk Mazurek <fryderyk.mazurek@gmail.com>
2021-02-28 13:04:15 +0100

    Nowe adresy

--- update-kanji.sh
+++ update-kanji.sh
@@ -1,18 +1,18 @@
-wget http://www.edrdg.org/kanjidic/kanjidic2.xml.gz
+wget http://ftp.edrdg.org/pub/Nihongo/kanjidic2.xml.gz
 gzip -d kanjidic2.xml.gz
 cat kanjidic2.xml | grep -v "<\!\-\- Entry for Kanji:" > kanjidic2.xml-temp
 mv kanjidic2.xml-temp kanjidic2.xml
 
 #
 
-wget ftp://ftp.monash.edu/pub/nihongo/radkfile.gz
+wget http://ftp.edrdg.org/pub/Nihongo/radkfile.gz
 gzip -d radkfile.gz
 iconv -f euc-jp -t utf-8 < radkfile > radkfile-utf8
 mv radkfile-utf8 radkfile
 
 #
 
-wget ftp://ftp.monash.edu/pub/nihongo/kradfile.gz
+wget http://ftp.edrdg.org/pub/Nihongo/kradfile.gz
 gzip -d kradfile.gz
 iconv -f euc-jp -t utf-8 < kradfile > kradfile-utf8
 mv kradfile-utf8 kradfile
