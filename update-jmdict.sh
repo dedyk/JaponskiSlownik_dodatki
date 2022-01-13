d848d0b517fd027f48668dc344d534d7a598f153
Fryderyk Mazurek <fryderyk.mazurek@gmail.com>
2022-01-13 08:34:19 +0100

    update

--- update-jmdict.sh
+++ update-jmdict.sh
@@ -1,14 +1,14 @@
 wget http://ftp.edrdg.org/pub/Nihongo/JMdict.gz
 
-gzip -d JMdict.gz
+gzip -f -d JMdict.gz
 
 wget http://ftp.edrdg.org/pub/Nihongo/JMdict_e.gz
 
-gzip -d JMdict_e.gz
+gzip -f -d JMdict_e.gz
 
 wget http://ftp.edrdg.org/pub/Nihongo/edict_sub.gz
 
-gzip -d edict_sub.gz
+gzip -f -d edict_sub.gz
 
 iconv -f euc-jp -t utf-8 < edict_sub > edict_sub-utf8
 
