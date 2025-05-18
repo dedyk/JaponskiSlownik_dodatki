32600b5d9c61e8b17621305b71950cbe2818743e
Fryderyk Mazurek <fryderyk.mazurek@gmail.com>
2025-05-18 12:33:22 +0200

    Dodanie dodatkowych kanji do kanjidic2

--- update-kanji.sh
+++ update-kanji.sh
@@ -11,14 +11,14 @@ mv kanjidic2.xml.temp2 kanjidic2.xml
 
 #
 
-#wget http://ftp.edrdg.org/pub/Nihongo/radkfile.gz
-#gzip -f -d radkfile.gz
-#iconv -f euc-jp -t utf-8 < radkfile > radkfile-utf8
-#mv radkfile-utf8 radkfile
+wget http://ftp.edrdg.org/pub/Nihongo/radkfile.gz
+gzip -f -d radkfile.gz
+iconv -f euc-jp -t utf-8 < radkfile > radkfile-utf8
+mv radkfile-utf8 radkfile
 
 #
 
-#wget http://ftp.edrdg.org/pub/Nihongo/kradfile.gz
-#gzip -f -d kradfile.gz
-#iconv -f euc-jp -t utf-8 < kradfile > kradfile-utf8
-#mv kradfile-utf8 kradfile
+wget http://ftp.edrdg.org/pub/Nihongo/kradfile.gz
+gzip -f -d kradfile.gz
+iconv -f euc-jp -t utf-8 < kradfile > kradfile-utf8
+mv kradfile-utf8 kradfile
