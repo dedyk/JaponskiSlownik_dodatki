a4c65ef3667333919e0d2794d3ff2c03ba51d990
Fryderyk Mazurek <fryderyk.mazurek@gmail.com>
2025-11-16 11:38:47 +0100

    update

--- jmdicts_download.sh
+++ jmdicts_download.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+cd jmdicts_updater
+
 ./update-jmdict.sh
 ./update-jmnedict.sh
 ./update-kanji.sh
+
+dir -1l
---
+++ jmdicts_update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd jmdicts_updater
+
+mv -f edict_sub-utf8 ..
+mv -f JMdict ..
+mv -f JMdict_e ..
+mv -f JMnedict.xml ..
+mv -f JMnedict.xml.bz2 ..
+mv -f kanjidic2.xml ..
+mv -f kradfile ..
+mv -f radkfile ..
