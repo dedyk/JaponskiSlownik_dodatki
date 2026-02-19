88e39ee92b8951f9acd27b32d0d9e6d2a54fb27e
Fryderyk Mazurek <fryderyk.mazurek@gmail.com>
2026-02-19 15:58:24 +0100

    update

---
+++ 01_jmdicts_backup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp -f edict_sub-utf8 jmdicts_updater/backup
+cp -f JMdict jmdicts_updater/backup
+cp -f JMdict_e jmdicts_updater/backup
+cp -f JMnedict.xml jmdicts_updater/backup
+cp -f JMnedict.xml.bz2 jmdicts_updater/backup
+cp -f kanjidic2.xml jmdicts_updater/backup
+cp -f kradfile jmdicts_updater/backup
+cp -f radkfile jmdicts_updater/backup
